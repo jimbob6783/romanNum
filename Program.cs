5ab981254a8291b5c392f4e4372a9d2fd89d8260
chris bridges <chris.bridges3@gmail.com>
2022-05-12 21:01:37 -0400

    ON TRACK!

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
 //  500=D
 // 1000=M
 
-Dictionary<string, int> romanNumeralsDictionary = new()
+Dictionary <string, int> romanNumeralsDictionary = new()
 {   
     { "I", 1 }, { "II", 2 }, { "III", 3 }, {"IV", 4 }, { "V", 5 }, { "VI", 6 }, { "VII", 7 }, { "VIII", 8 }, { "IX", 9 },
     { "X", 10 }, { "XX", 20 }, {"XXX", 30}, {"XL", 40}, { "L", 50 }, { "LX", 60 }, { "LXX", 70 }, { "LXXX", 80 }, { "XC", 90 },
@@ -16,10 +16,13 @@ Dictionary<string, int> romanNumeralsDictionary = new()
 };
 
 Console.WriteLine("Type in a number between 1-3999 to get the Roman Numberal Equivalent!");
-int Number = Convert.ToInt32(Console.ReadLine());
-if (Number == 0) Console.WriteLine("Must be bigger than Zero");
-if (Number > 3999) Console.WriteLine("Smaller number please");
-
-//if (Number == int) Console.WriteLine(romanNumeralsDictionary);
+if (int.TryParse(Console.ReadLine(), out int Number))
+{if (Number <= 0) { Console.WriteLine("Must be bigger than Zero"); return; }
+    if (Number > 3999) { Console.WriteLine("Smaller number please"); return; }
     
+} else Console.WriteLine("Was not a number");
 
+
+
+
+     
